1fd22abaa733137524b372911bd0062bf80112c5
aspencass <caspenw1@gmail.com>
2022-07-29 15:40:39 -0600

    add index.html, add script.js, add style.css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "assembled by Cassidy Wagner";
+h2.textContent = "Who is Cassidy Wagner?";
 
 document.querySelector("body").appendChild(h2);
 
